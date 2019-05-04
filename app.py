2baa093653024f36181fd27360aa59d2708f4b6a
rachitrawat <rachit.rawatr@gmail.com>
2019-05-04 20:06:23 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,196 @@
+import getpass
+import json
+import os
+import random
+import string
+import subprocess
+
+from flask import Flask, render_template, request
+import utils
+
+app = Flask(__name__)
+
+db_path = "/home/r/PycharmProjects/flask_chat_server/client_db.json"
+user_dict = {}
+available_wallets = [x for x in range(1, 10000)]
+if os.path.isfile(db_path):
+    db_content = utils.read_file(db_path)[0]
+    user_dict = json.loads(db_content)
+    used_wallets = []
+    # update available wallets
+    for k, v in user_dict.items():
+        used_wallets.append(int(v["wallet"][4:]))
+    available_wallets = list(set(available_wallets) - set(used_wallets))
+else:
+    utils.write_file(db_path, "{}")
+
+FABRIC_DIR = "/home/" + getpass.getuser() + "/WebstormProjects/hyperledger_project/fabchat/javascript/"
+NODE_PATH = "/home/" + getpass.getuser() + "/node/bin/node"
+DEBUG = True
+
+
+def registerUser(user):
+    output = "dummy"
+
+    try:
+        output = subprocess.check_output(
+            [NODE_PATH, FABRIC_DIR + "registerUser.js", user]).decode(
+            'ascii').split()
+
+    except:
+        pass
+
+    if DEBUG:
+        print(' '.join(output))
+
+    if output[len(output) - 1] == "wallet":
+        return 0
+    else:
+        return 1
+
+
+def createMsg(req_obj):
+    user = req_obj['email']
+    msgText = req_obj['msgtext']
+    msgText = '__'.join(msgText.split())
+    output = "dummy"
+
+    try:
+        output = subprocess.check_output(
+            [NODE_PATH, FABRIC_DIR + "invoke.js", "createMsg", msgText, user_dict[user]["wallet"], user]).decode(
+            'ascii').split()
+    except:
+        pass
+
+    if DEBUG:
+        print(' '.join(output))
+
+    if output[len(output) - 1] == "submitted":
+        return 0
+    else:
+        return 1
+
+
+def handle_setup(req_obj, flag):
+    uid = req_obj['email']
+
+    # registration
+    if flag == "True":
+        # if user already exists, fail
+        # if no more wallets available, fail
+        if uid in user_dict or not available_wallets:
+            utils.send_verification_email(uid, user_dict[uid]['pwd'])
+            return 1
+        # else assign user with a wallet
+        else:
+            pwd = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(4))
+            utils.send_verification_email(uid, pwd)
+            wallet_id = random.choice(available_wallets)
+            available_wallets.remove(wallet_id)
+            user_dict[uid] = {"pwd": pwd, "wallet": "user" + str(wallet_id)}
+            # initial registration msg
+            if registerUser(user_dict[uid]['wallet']) != 0 or createMsg("$HELLO$" + " " + uid) != 0:
+                user_dict.pop(uid)
+                available_wallets.append(wallet_id)
+                return 1
+            utils.write_file(db_path, json.dumps(user_dict))
+            return 0
+    # login
+    elif flag == "False":
+        pwd = req_obj['pwd']
+        # if user does not exist or pwd does not match, fail
+        if uid not in user_dict or pwd != user_dict[uid]["pwd"]:
+            return 1
+        else:
+            # if user exists and pwd matches, pass
+            if pwd == user_dict[uid]["pwd"]:
+                return 0
+            # if user exists and pwd does not match, fail
+            else:
+                return 1
+
+
+def flagMsg(data):
+    msgID = data.split()[0]
+    user = data.split()[1]
+    output = "dummy"
+
+    try:
+        output = subprocess.check_output(
+            [NODE_PATH, FABRIC_DIR + "invoke.js", "flagMsg", msgID, user_dict[user]["wallet"]]).decode(
+            'ascii').split()
+    except:
+        pass
+
+    if DEBUG:
+        print(' '.join(output))
+
+    if output[len(output) - 1] == "submitted":
+        return 0
+    else:
+        return 1
+
+
+def queryAllMsgs(req_obj):
+    msgID = "-1"
+    user = req_obj['email']
+    output = "dummy"
+
+    try:
+        output = subprocess.check_output(
+            [NODE_PATH, FABRIC_DIR + "query.js", msgID, user_dict[user]["wallet"]]).decode(
+            'ascii').split()
+    except:
+        pass
+
+    if DEBUG:
+        print(' '.join(output))
+
+    if output != "dummy" and output[6] == "evaluated,":
+        return 0, json.loads(output[len(output) - 1])
+    else:
+        return 1, " "
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/', methods=['POST'])
+def home_post():
+    res = handle_setup(request.form, "False")
+    if res == 0:
+        if request.form['submit_button'] == 'Post Message':
+            if res == 0:
+                createMsg(request.form)
+                return "Message posted successfully!"
+            else:
+                return "Failed to post message!"
+        elif request.form['submit_button'] == 'See All Messages':
+            res, raw_query_str = queryAllMsgs(request.form)
+            if res == 0:
+                query_lst = utils.format_query(raw_query_str)
+                return render_template('show_query.html', query_lst=query_lst)
+            else:
+                return "Failed to query messages!"
+    else:
+        return "Invalid email/password."
+
+
+@app.route('/register', methods=['POST'])
+def register_post():
+    res = handle_setup(request.form, "True")
+    if res == 0:
+        return "Registration successful! Check your email inbox/spam for password."
+    else:
+        return "Registration failed! You're already registered. Check your email's inbox/spam folder for password."
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ utils.py
@@ -0,0 +1,116 @@
+import ast
+import os
+import random
+import smtplib
+
+
+def send_file(file_name, socket_obj):
+    """ Robust file transfer method """
+    BYTES_RECV = 1024
+
+    statinfo = os.stat(file_name)
+    file_size = statinfo.st_size
+
+    # encode filesize as 32 bit binary
+    fsize_b = bin(file_size)[2:].zfill(32)
+    socket_obj.send(fsize_b.encode('ascii'))
+
+    f = open(file_name, 'rb')
+
+    while file_size >= BYTES_RECV:
+        l = f.read(BYTES_RECV)
+        socket_obj.send(l)
+        file_size -= BYTES_RECV
+
+    if file_size > 0:
+        l = f.read(file_size)
+        socket_obj.send(l)
+
+    f.close()
+
+
+def recv_file(file_name, socket_obj):
+    """ Robust file transfer method """
+    BYTES_RECV = 1024
+
+    fsize_b = socket_obj.recv(32).decode('ascii')
+    fsize = int(fsize_b, 2)
+
+    f = open(file_name, 'wb')
+    file_size = fsize
+
+    while file_size >= BYTES_RECV:
+        buff = bytearray()
+        while len(buff) < BYTES_RECV:
+            buff.extend(socket_obj.recv(BYTES_RECV - len(buff)))
+        f.write(buff)
+        file_size -= BYTES_RECV
+
+    if file_size > 0:
+        buff = bytearray()
+        while len(buff) < file_size:
+            buff.extend(socket_obj.recv(file_size - len(buff)))
+        f.write(buff)
+
+    f.close()
+
+
+def write_file(file_name, string):
+    with open(file_name, 'w+') as f:
+        f.write(string)
+
+
+def read_file(file_name):
+    with open(file_name) as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+    return content
+
+
+def ascii_len(s):
+    """ returns string size in bytes """
+    return len(s.encode('ascii'))
+
+
+def send_string(string, socket_obj):
+    size = ascii_len(string)
+
+    # encode string size as 32 bit binary
+    fsize_b = bin(size)[2:].zfill(32)
+    socket_obj.send(fsize_b.encode('ascii'))
+
+    socket_obj.send(string.encode('ascii'))
+
+
+def recv_string(socket_obj):
+    fsize_b = socket_obj.recv(32).decode('ascii')
+    fsize = int(fsize_b, 2)
+
+    return socket_obj.recv(fsize).decode('ascii')
+
+
+def send_verification_email(recv_addr, pwd):
+    try:
+        session = smtplib.SMTP('smtp.gmail.com', 587)
+        session.starttls()
+        session.login("fabchat.service@gmail.com", "fabchat-test")
+        message = "Subject: Hi there!\n\nYour password is " + pwd + "."
+        session.sendmail("fabchat.service@gmail.com", recv_addr, message)
+        session.quit()
+    except Exception as e:
+        print(e)
+        return "-1"
+
+
+def format_query(raw_query_str):
+    data = ast.literal_eval(raw_query_str)
+    query_lst = []
+    for idx, val in enumerate(data):
+        val['Record']['msgText'] = ' '.join(val['Record']['msgText'].split('__'))
+        if 'owner' in val['Record']:
+            msg = val['Key'] + " " + val['Record']['msgText'] + " " + val['Record']['owner'] + "\n"
+        else:
+            msg = val['Key'] + " " + val['Record']['msgText']
+        query_lst.append(msg)
+
+    return query_lst
